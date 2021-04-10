7bb84823a2c5bac5735e950d0b740e97f70d7f92
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2021-04-10 05:31:41 -0500

    d

--- client/src/App.js
+++ client/src/App.js
@@ -33,26 +33,26 @@ class App extends Component {
     this.setState(userObject)
   }
 
-  getUser() {
-    router.get('/').then(response => {
-      console.log('Get user response: ')
-      console.log(response.data)
-      if (response.data.user) {
-        console.log('Get User: There is a user saved in the server session: ')
+  // getUser() {
+  //   router.get('/').then(response => {
+  //     console.log('Get user response: ')
+  //     console.log(response.data)
+  //     if (response.data.user) {
+  //       console.log('Get User: There is a user saved in the server session: ')
 
-        this.setState({
-          loggedIn: true,
-          name: response.data.user.name
-        })
-      } else {
-        console.log('Get user: no user');
-        this.setState({
-          loggedIn: false,
-          name: null
-        })
-      }
-    })
-  }
+  //       this.setState({
+  //         loggedIn: true,
+  //         name: response.data.user.name
+  //       })
+  //     } else {
+  //       console.log('Get user: no user');
+  //       this.setState({
+  //         loggedIn: false,
+  //         name: null
+  //       })
+  //     }
+  //   })
+  // }
 
  render() {
   return (
