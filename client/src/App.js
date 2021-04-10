6cb89d116bfa7e2574ff53e11ef85772bdc8901b
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2021-04-10 04:51:23 -0500

    debugging

--- client/src/App.js
+++ client/src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
   }
 
   getUser() {
-    axios.get('/user/').then(response => {
+    axios.get('/').then(response => {
       console.log('Get user response: ')
       console.log(response.data)
       if (response.data.user) {
